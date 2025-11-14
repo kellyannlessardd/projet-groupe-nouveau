25ee3564a28742fa97790c47ae69df5c33c910d3
a10ngy <amugi020@uottawa.ca>
2025-11-14 13:33:45 -0500

    ajouter inverse_kinematics

---
+++ inverse_kinematics.py
@@ -0,0 +1,12 @@
+import math
+
+def calculate_angles(x, y):
+    """
+    Calcule les angles d’articulation à partir des coordonnées X et Y.
+    Retourne : (alpha, beta)
+    alpha = angle à l’épaule, beta = angle au coude
+    """
+    # [À compléter plus tard avec les équations trigonométriques]
+    alpha = 0.0
+    beta = 0.0
+    return alpha, beta
